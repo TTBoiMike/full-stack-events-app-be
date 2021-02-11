cc21cbe464fdae2ac2e136fb097981de1c919ed0
TTBoiMike <michaelthorpe747@hotmail.com>
2021-02-11 19:29:43 +0000

    deploy to heroku

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.port || 3001
+const port = process.env.PORT || 3001
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
